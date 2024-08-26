58c474b920d9786a59f20a55d95e62f04ca14def
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-08-26 10:23:40 -0500

    Setting global colors for the app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,17 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      'blue': '#0079FF',
+      'medium-grey': '#697C9A',
+      'blue-grey': '#4B6A9B',
+      'dark-grey': '#2B3442',
+      'light-grey': '#F6F8FF',
+      'almost-white': '#FEFEFE',
+      'white': '#FFFFFF',
+      'almost-black': '#141D2F',
+      'very-dark-grey': '#1E2A47'
+    }
   },
   plugins: [],
 };
