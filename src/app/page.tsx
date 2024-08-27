d0a4dcb773ed9735c53edf1c1ed8593fbb728e49
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-08-27 14:16:27 -0500

    Context for accessing to searchInputValue and user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,17 @@ import Image from "next/image";
 import Header from "./components/Header";
 import Search from "./components/Search";
 import Profile from "./components/Profile";
+import { UserProvider } from "./context/Context";
+import { AppProps } from "next/app";
 
-export default function Home() {
+export default function Home({Component, pageProps}: AppProps) {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <Header/>
-      <Search/>
-      <Profile/>
-    </main>
+    <UserProvider>
+      <main className="flex min-h-screen p-8  md:p-16 flex-col lg:px-48 xl:px-64">
+        <Header/>
+        <Search/>
+        <Profile/>
+      </main>
+    </UserProvider>
   );
 }
