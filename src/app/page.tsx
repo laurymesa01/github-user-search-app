7500b196bf1648ac84a3d02405c6c5ffb04970fd
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-09-03 11:04:28 -0500

    Fixing user attributes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import { ChangeEvent, useEffect, useState } from "react";
 import axios from "axios";
 import { Type, Users } from "./services/user";
 import Loader from "./components/Loader";
-import { notFound } from "next/navigation";
 
 
 export default function Home() {
@@ -15,37 +14,37 @@ export default function Home() {
   const apiUrl = 'https://api.github.com';
   const [input, setInput] = useState('octocat');
   const [user, setUser] = useState<Users>({
-    login: '',
-    id: 0,
-    node_id: '',
-    avatar_url: '',
-    gravatar_id: '',
-    url: '',
-    html_url: '',
-    followers_url: '',
-    following_url: '',
-    gists_url: '',
-    starred_url: '',
-    subscriptions_url: '',
-    organizations_url: '',
-    repos_url: '',
-    events_url: '',
+    login:               '',
+    name:                '',
+    id:                  0,
+    node_id:             '',
+    avatar_url:          '',
+    gravatar_id:         '',
+    url:                 '',
+    html_url:            '',
+    followers_url:       '',
+    following_url:       '',
+    gists_url:           '',
+    starred_url:         '',
+    subscriptions_url:   '',
+    organizations_url:   '',
+    repos_url:           '',
+    events_url:          '',
     received_events_url: '',
-    type: Type.User,
-    site_admin: false,
-    name: "",
-    company: '',
-    blog: '',
-    location: '',
-    email: null,
-    hireable: null,
-    bio: null,
-    twitter_username: null,
-    public_repos: 0,
-    public_gists: 0,
-    followers: 0,
-    following: 0,
-    created_at: new Date().toISOString()
+    type:                Type.User,
+    site_admin:          false,
+    company:             '',
+    blog:                '',
+    location:            '',
+    email:               null,
+    hireable:            null,
+    bio:                 null,
+    twitter_username:    null,
+    public_repos:        0,
+    public_gists:        0,
+    followers:           0,
+    following:           0,
+    created_at:          new Date().toISOString(),
   });
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
